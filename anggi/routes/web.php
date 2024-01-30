0ea3107eb6e6a56df8b2447ee4a15d4c4f2093de
anggi135 <jagoshell@gmail.com>
2024-01-31 04:37:03 +0700

    new file:   anggi/resources/views/layouts/custom.blade.php 	new file:   anggi/resources/views/logs/forgot.blade.php 	new file:   anggi/resources/views/logs/login.blade.php 	new file:   anggi/resources/views/logs/regis.blade.php 	new file:   anggi/resources/views/logs/reset.blade.php 	modified:   anggi/routes/web.php

--- anggi/routes/web.php
+++ anggi/routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('dashboard.home');
 });
+Route::get('/login', function () {
+    return view('logs.login');
+});
+Route::get('/forgot', function () {
+    return view('logs.forgot');
+});
+Route::get('/regis', function () {
+    return view('logs.regis');
+});
+Route::get('/reset', function () {
+    return view('logs.reset');
+});
