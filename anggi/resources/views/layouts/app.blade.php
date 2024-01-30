1da766e6a05bc99b422dd114c0bbecb83b9a3f6a
anggi135 <jagoshell@gmail.com>
2024-01-31 03:13:18 +0700

    Fixing script js and, Title, section 	new file:   resources/views/dashboard/home.blade.php 	modified:   resources/views/layouts/app.blade.php 	modified:   resources/views/posisi/sidebar.blade.php 	modified:   routes/web.php

--- anggi/resources/views/layouts/app.blade.php
+++ anggi/resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
-  <title>Blank Page &mdash; Stisla</title>
+  <title>@yield('title') - Indofaststar</title>
 
   <!-- General CSS Files -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
@@ -50,13 +50,13 @@
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.nicescroll/3.7.6/jquery.nicescroll.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
-  <script src="{{asset('/assets/js/stisla.js')}}></script>
+  <script src="{{asset('assets/js/stisla.js')}}"></script>
 
   <!-- JS Libraies -->
 
   <!-- Template JS File -->
-  <script src="{{asset('/assets/js/scripts.js')}}"></script>
-  <script src="{{asset('/assets/js/custom.js')}}"></script>
+  <script src="{{asset('assets/js/scripts.js')}}"></script>
+  <script src="{{asset('assets/js/custom.js')}}"></script>
 
   <!-- Page Specific JS File -->
 </body>
