7cee338be3df60e8bea492c6d7586125344645b2
anggi135 <jagoshell@gmail.com>
2024-01-31 01:54:27 +0700

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch 02-layout-file
     Your branch is up to date with 'origin/02-layout-file'.
    
     Changes to be committed:
    	modified:   anggi/resources/views/layouts/app.blade.php
    	new file:   anggi/resources/views/posisi/footer.blade.php

--- anggi/resources/views/layouts/app.blade.php
+++ anggi/resources/views/layouts/app.blade.php
@@ -39,12 +39,7 @@
         </section>
       </div>
       <footer class="main-footer">
-        <div class="footer-left">
-          Copyright &copy; 2018 <div class="bullet"></div> Design By <a href="https://nauv.al/">Muhamad Nauval Azhar</a>
-        </div>
-        <div class="footer-right">
-          2.3.0
-        </div>
+        @include('posisi.footer')
       </footer>
     </div>
   </div>
